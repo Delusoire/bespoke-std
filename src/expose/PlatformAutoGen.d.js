16ac9ae8918ae73eeb61eb51f6541e47b26f6bc3
Delusoire <71462480+Delusoire@users.noreply.github.com>
2024-03-12 15:02:44 +0000

    y

--- src/expose/PlatformAutoGen.d.js
+++ src/expose/PlatformAutoGen.d.js
@@ -1,6 +1,6 @@
 /*
 const HU = await import("/hooks/util.js")
-const MSI = await import("/modules/Delusoire/std/index.js")
+const MSI = await import("/modules/Delusoire/stdlib/index.js")
 HU.visited.clear()
 MSI.S.Platform.getClipboardAPI().copy(HU.type(MSI.S.Platform, "PlatformAutoGen"))
-*/ 
+*/
