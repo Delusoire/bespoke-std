3169e4fd2fe5420d7aa8becf47c87f3884c9e62e
harbassan <harbassan@hotmail.com>
2024-03-16 23:32:24 +1300

    improve dropdown

--- lib/components/Dropdown.tsx
+++ lib/components/Dropdown.tsx
@@ -3,74 +3,85 @@ import { S } from "../../index.js";
 import { SVGIcons } from "../../src/static.js";
 
 const CheckIcon = () =>
-	createIconComponent({
-		icon: SVGIcons.check,
-	});
+    createIconComponent({
+        icon: SVGIcons.check,
+    });
 
 interface MenuItemProps {
-	option: string;
-	isActive: boolean;
-	onSwitch: (option: string) => void;
-	children: React.ReactNode;
+    option: string;
+    isActive: boolean;
+    onSwitch: (option: string) => void;
+    children: React.ReactNode;
 }
 const DropdownMenuItem = ({ option, isActive, onSwitch, children }: MenuItemProps) => {
-	const activeStyle = {
-		backgroundColor: "rgba(var(--spice-rgb-selected-row),.1)",
-	};
+    const activeStyle = {
+        backgroundColor: "rgba(var(--spice-rgb-selected-row),.1)",
+    };
 
-	return (
-		<S.ReactComponents.MenuItem
-			trigger="click"
-			onClick={() => onSwitch(option)}
-			data-checked={isActive}
-			trailingIcon={isActive ? <CheckIcon /> : undefined}
-			style={isActive ? activeStyle : undefined}
-		>
-			{children}
-		</S.ReactComponents.MenuItem>
-	);
+    return (
+        <S.ReactComponents.MenuItem
+            trigger="click"
+            onClick={() => onSwitch(option)}
+            data-checked={isActive}
+            trailingIcon={isActive ? <CheckIcon /> : undefined}
+            style={isActive ? activeStyle : undefined}
+        >
+            {children}
+        </S.ReactComponents.MenuItem>
+    );
 };
 
 export type DropdownOptions = Record<string, React.ReactNode>;
 
 interface DropdownMenuProps<O extends DropdownOptions> {
-	options: O;
-	activeOption: keyof O;
-	onSwitch: (option: keyof O) => void;
+    options: O;
+    activeOption: keyof O;
+    onSwitch: (option: keyof O) => void;
 }
 export default function <O extends DropdownOptions>({ options, activeOption, onSwitch }: DropdownMenuProps<O>) {
-	const { ContextMenu, Menu, TextComponent } = S.ReactComponents;
+    const { ContextMenu, Menu, TextComponent } = S.ReactComponents;
 
-	const DropdownMenu = props => {
-		return (
-			<Menu {...props}>
-				{Object.entries(options).map(([option, children]) => (
-					<DropdownMenuItem option={option} isActive={option === activeOption} onSwitch={onSwitch}>
-						{children}
-					</DropdownMenuItem>
-				))}
-			</Menu>
-		);
-	};
+    const DropdownMenu = props => {
+        return (
+            <Menu {...props}>
+                {Object.entries(options).map(([option, children]) => (
+                    <DropdownMenuItem option={option} isActive={option === activeOption} onSwitch={onSwitch}>
+                        {children}
+                    </DropdownMenuItem>
+                ))}
+            </Menu>
+        );
+    };
 
-	return (
-		<ContextMenu menu={<DropdownMenu />} trigger="click">
-			<button className="x-sortBox-sortDropdown" type="button" role="combobox" aria-expanded="false">
-				{/* <TextComponent variant="mesto" semanticColor="textSubdued">
-					{options[activeOption]}
-				</TextComponent> */}
-				<svg
-					role="img"
-					height="16"
-					width="16"
-					aria-hidden="true"
-					className="Svg-img-16 Svg-img-16-icon Svg-img-icon Svg-img-icon-small"
-					viewBox="0 0 16 16"
-					data-encore-id="icon"
-				>
-					<path d="m14 6-6 6-6-6h12z" />
-				</svg>
-			</button>
-		</ContextMenu>
-	);
+    if (Object.keys(options).length === 1) {
+        return (
+            <button className="x-sortBox-sortDropdown" type="button" role="combobox" aria-expanded="false">
+                <TextComponent variant="mesto" semanticColor="textSubdued">
+                    {options[activeOption]}
+                </TextComponent>
+            </button>
+        );
+    }
+
+    return (
+        <ContextMenu menu={<DropdownMenu />} trigger="click">
+            <button className="x-sortBox-sortDropdown" type="button" role="combobox" aria-expanded="false">
+                <TextComponent variant="mesto" semanticColor="textSubdued">
+                    {options[activeOption]}
+                </TextComponent>
+                <svg
+                    role="img"
+                    height="16"
+                    width="16"
+                    aria-hidden="true"
+                    className="Svg-img-16 Svg-img-16-icon Svg-img-icon Svg-img-icon-small"
+                    viewBox="0 0 16 16"
+                    data-encore-id="icon"
+                    fill="currentColor"
+                >
+                    <path d="m14 6-6 6-6-6h12z" />
+                </svg>
+            </button>
+        </ContextMenu>
+    );
 }
