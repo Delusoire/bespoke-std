8860de90f859f6963762473a7db3d9a3d07b6aac
Delusoire <deluso7re@outlook.com>
2024-03-18 10:36:12 +0000

    feat(deps): add startCase export

--- deps.ts
+++ deps.ts
@@ -5,5 +5,6 @@ export const _ = ld;
 import ld_fp from "https://esm.sh/lodash/fp";
 export const fp = ld_fp;
 
+export { default as startCase } from "https://esm.sh/lodash/startCase";
 export { default as kebabCase } from "https://esm.sh/lodash/kebabCase";
 export { default as capitalize } from "https://esm.sh/lodash/fp/capitalize";
