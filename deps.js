ec7420e405b6c9a50d8b847b62f607543f621a8e
Delusoire <71462480+Delusoire@users.noreply.github.com>
2024-03-18 21:06:07 +0000

    chore: build

--- deps.js
+++ deps.js
@@ -3,5 +3,6 @@ import ld from "https://esm.sh/lodash";
 export const _ = ld;
 import ld_fp from "https://esm.sh/lodash/fp";
 export const fp = ld_fp;
+export { default as startCase } from "https://esm.sh/lodash/startCase";
 export { default as kebabCase } from "https://esm.sh/lodash/kebabCase";
 export { default as capitalize } from "https://esm.sh/lodash/fp/capitalize";
